818d375c1aed5a6a93f9ea34c6b819d4a9602653
Oleg Melnyk <olegmelnyk237@gmail.com>
2025-03-06 00:05:42 +0200

    Moved project to backend folder

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -40,7 +40,7 @@ namespace BookStore
                 app.UseSwagger();
                 app.UseSwaggerUI(c =>
                 {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookStore V1");
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookStoreV1");
                     c.RoutePrefix = "";
                 });
             }
